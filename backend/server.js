87a071efeea15bc5c593364a294bdc249d2a8072
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-07-04 12:14:04 +0530

    create the server and connect with db

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import server from "./index.js";
+import dbConnect from "./config/db.js";
+
+const PORT = process.env.PORT || 3000;
+
+async function startServer() {
+  try {
+    await dbConnect();
+    server.listen(PORT, () => {
+      console.log(`✅ Server running on ${PORT} port`);
+    });
+  } catch (error) {
+    console.log("❌ Error starting server", error);
+    process.exit(1);
+  }
+}
