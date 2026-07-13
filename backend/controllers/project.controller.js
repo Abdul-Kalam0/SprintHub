2839d8c0121ba07ab732baabc13bf9f815664370
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-07-13 11:23:26 +0530

    feat(project): add update and delete project

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -22,7 +22,70 @@ export const createProject = async (req, res, next) => {
     next(error);
   }
 };
-export const getAllProjects = async (req, res, next) => {};
-export const getProject = async (req, res, next) => {};
-export const updateProject = async (req, res, next) => {};
-export const deleteProject = async (req, res, next) => {};
+export const getAllProjects = async (req, res, next) => {
+  try {
+    const projects = await getAllProjectsService(
+      req.params.workspaceId,
+      req.user._id,
+    );
+    res.status(200).json({
+      success: true,
+      message: "Projects fetched successfully",
+      data: projects,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const getProject = async (req, res, next) => {
+  try {
+    const project = await getProjectsService(
+      req.params.workspaceId,
+      req.user._id,
+      req.params.projectId,
+    );
+    res.status(200).json({
+      success: true,
+      message: "Project fetched successfully",
+      data: project,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateProject = async (req, res, next) => {
+  try {
+    const updatedProject = await updateProjectService(
+      req.params.workspaceId,
+      req.user._id,
+      req.params.projectId,
+      req.body,
+    );
+    res.status(200).json({
+      success: true,
+      message: "Project updated successfully",
+      data: updatedProject,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteProject = async (req, res, next) => {
+  try {
+    const deletedProject = await deleteProjectService(
+      req.params.workspaceId,
+      req.user._id,
+      req.params.projectId,
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Project deleted successfully",
+      data: deletedProject,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/services/project.service.js
+++ backend/services/project.service.js
@@ -10,7 +10,10 @@ export const createProject = async (workspaceId, ownerId, data) => {
   }
 
   //due date must be after start date
-  if (startDate && dueDate && startDate > dueDate) {
+  const start = startDate ? new Date(startDate) : null;
+  const due = dueDate ? new Date(dueDate) : null;
+
+  if (start && due && start > due) {
     const error = new Error("Due date must be after start date");
     error.statusCode = 400;
     throw error;
@@ -44,15 +47,149 @@ export const createProject = async (workspaceId, ownerId, data) => {
     workspace: workspaceId,
     createdBy: ownerId,
     status,
-    startDate,
-    dueDate,
+    startDate: start,
+    dueDate: due,
     description,
     isArchived,
   });
 
   return newProject;
 };
-export const getAllProjects = async () => {};
-export const getProject = async () => {};
-export const updateProject = async () => {};
-export const deleteProject = async () => {};
+export const getAllProjects = async (workspaceId, ownerId) => {
+  // checking whether this workspace is of current loggedin user
+  const existingWorkspace = await WorkspaceModel.findOne({
+    _id: workspaceId,
+    owner: ownerId,
+  });
+
+  if (!existingWorkspace) {
+    const error = new Error("Workspace not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  const projects = await ProjectModel.find({
+    workspace: workspaceId,
+  }).sort({ createdAt: -1 });
+
+  return projects;
+};
+export const getProject = async (workspaceId, ownerId, projectId) => {
+  // checking whether this workspace is of current loggedin user
+  const existingWorkspace = await WorkspaceModel.findOne({
+    _id: workspaceId,
+    owner: ownerId,
+  });
+
+  if (!existingWorkspace) {
+    const error = new Error("Workspace not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  const project = await ProjectModel.findOne({
+    workspace: workspaceId,
+    _id: projectId,
+  });
+  if (!project) {
+    const error = new Error("Project not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  return project;
+};
+export const updateProject = async (workspaceId, ownerId, projectId, data) => {
+  const { name, description, status, isArchived, startDate, dueDate } = data;
+
+  const existingWorkspace = await WorkspaceModel.findOne({
+    _id: workspaceId,
+    owner: ownerId,
+  });
+  if (!existingWorkspace) {
+    const error = new Error("Workspace not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  const existingProject = await ProjectModel.findOne({
+    workspace: workspaceId,
+    _id: projectId,
+  });
+  if (!existingProject) {
+    const error = new Error("Project not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  const finalName = name ?? existingProject.name;
+  if (existingProject.name !== finalName) {
+    const duplicateProject = await ProjectModel.findOne({
+      workspace: workspaceId,
+      name: finalName,
+    });
+    if (duplicateProject) {
+      const error = new Error("Project already exist");
+      error.statusCode = 409;
+      throw error;
+    }
+  }
+
+  const finalStartDate = startDate ?? existingProject.startDate;
+  const finalDueDate = dueDate ?? existingProject.dueDate;
+  const start = finalStartDate ? new Date(finalStartDate) : null;
+  const due = finalDueDate ? new Date(finalDueDate) : null;
+
+  if (start && due && start > due) {
+    const error = new Error("Due date must be after start date");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  const updatedProject = await ProjectModel.findOneAndUpdate(
+    {
+      _id: projectId,
+      workspace: workspaceId,
+    },
+    {
+      name: finalName,
+      description: description ?? existingProject.description,
+      status: status ?? existingProject.status,
+      isArchived: isArchived ?? existingProject.isArchived,
+      dueDate: due,
+      startDate: start,
+    },
+    {
+      new: true,
+    },
+  );
+  return updatedProject;
+};
+export const deleteProject = async (workspaceId, ownerId, projectId) => {
+  const existingWorkspace = await WorkspaceModel.findOne({
+    _id: workspaceId,
+    owner: ownerId,
+  });
+  if (!existingWorkspace) {
+    const error = new Error("Workspace not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  const existingProject = await ProjectModel.findOne({
+    workspace: workspaceId,
+    _id: projectId,
+  });
+  if (!existingProject) {
+    const error = new Error("Project not found");
+    error.statusCode = 404;
+    throw error;
+  }
+
+  const deletedProject = await ProjectModel.findOneAndDelete({
+    workspace: workspaceId,
+    _id: projectId,
+  });
+
+  return deletedProject;
+};
